90043ef7cbba181783c1e0d5ec7c18062a6405e0
Md. Liaz Mahamud <mdliazmahamud@gmail.com>
2021-01-31 15:25:27 +0600

    authcurd Project

---
+++ resources/views/admin/slider/index.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+@section('title', 'Slider')
+
+@push('css')
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css">
+@endpush
+
+@section('content')
+
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header card-header-primary">
+                            <h4 class="card-title ">All Slider</h4>
+
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table id="example" class="table table-striped table-bordered" style="width:100%">
+                                    <thead class=" text-primary">
+                                    <th>ID</th>
+                                    <th>Title</th>
+                                    <th>Sub Title</th>
+                                    <th>Image</th>
+                                    <th>Action</th>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($sliders as $key=>$slider)
+                                        <tr>
+                                            <td>{{ $key+1 }}</td>
+                                            <td>{{ $slider->title }}</td>
+                                            <td>{{ $slider->sub_title }}</td>
+                                            <td>{{ $slider->image }}</td>
+                                            <td>
+                                                <a href="#" class="btn btn-info btn-sm">Edit</a>
+                                                <a href="#" class="btn btn-danger btn-sm">Delete</a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@push('js')
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap4.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable();
+        } );
+    </script>
+@endpush
+
